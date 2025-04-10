a849f956c120427b3d3832e6f96289c1eb966b71
Ajjubook <rajmahimaurya@gmail.com>
2025-04-10 16:25:08 +0530

    Dashboard: Update App Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,6 @@ import LoginPage from "./views/LoginPage"
 import DashboardLayout from "./components/layouts/DashboardLayout";;
 import "./index.css";
 
-// Dashboard views
-// import RealtimeView from "./views/dashboard/RealtimeView";
-// import HistoricalView from "./views/dashboard/HistoricalView";
-// import ThreatIntelView from "./views/dashboard/ThreatIntelView";
-// import ConfigView from "./views/dashboard/ConfigView";
-// import UploadLogsView from "./views/dashboard/UploadLogsView";
-// import ProfileView from "./views/dashboard/ProfileView";
 
 function App() {
   return (
@@ -25,12 +18,6 @@ function App() {
 
       {/* -----------------------dashboard routes------------------------------------ */}
       <Route path="/dashboard" element={<DashboardLayout />}>
-        {/* <Route path="realtime" element={<RealtimeView />} /> */}
-        {/* <Route path="historical" element={<HistoricalView />} />
-        <Route path="threat-intel" element={<ThreatIntelView />} />
-        <Route path="config" element={<ConfigView />} />
-        <Route path="upload-logs" element={<UploadLogsView />} />
-        <Route path="profile" element={<ProfileView />} /> */}
       </Route>
 
       </Routes>
